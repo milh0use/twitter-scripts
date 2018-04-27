0b805205c5a41607eb7406248dd810c7bb6d5d3c
shirschorn <steven.hirschorn@nationalarchives.gsi.gov.uk>
2018-04-27 12:15:11 +0100

    Add config and attempt to get a user's friends list
    You need to create a twitter-auth.config file in the project root which contains your twitter authentication credentials in the form:
    [main]
    consumer_key=<consumer_key>
    consumer_secret=<consumer_secret>
    access_token_key=<access_token_key>
    access_token_secret=<access_token_secret>
    
    These credentials can be created/downloaded by creating an app at:
    https://apps.twitter.com/

--- twitter-get.py
+++ twitter-get.py
@@ -1,12 +1,25 @@
 import twitter
 import configparser
+import argparse
+
+parser = argparse.ArgumentParser(description='Get the list of "friends" of a twitter user and add them to a named collection')
+parser.add_argument("-u", "--user",
+                    help='The user whose friends list should be exported')
+
+args = parser.parse_args()
+
+user = ''
+if hasattr(args, 'user') and vars(args)['user'] != None:
+    user = vars(args)['user']
 
 config = configparser.ConfigParser()
 config.read('twitter-auth.config')
-
 
 api = twitter.Api(consumer_key=config['main']['consumer_key'],
                       consumer_secret=config['main']['consumer_secret'],
                       access_token_key=config['main']['access_token_key'],
                       access_token_secret=config['main']['access_token_secret'])
 
+print("Scanning Twitter Friends list of " + user)
+users = api.GetFriends(user_id=user)
+print([u.name for u in users])
